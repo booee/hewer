053b44cba03a125fc697c58bc6e9fb724b944900
brad-bowie <broanius@gmail.com>
2015-06-05 12:27:36 -0400

    trying to work this out

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
-import (
-)
 
 func main() {
-	//ParseFile("/Users/bradbowie/Desktop/metrics-calamp-blue-20501.log");
+	// ParseFile("/Users/bradbowie/Desktop/metrics-calamp-blue-20501.log");
+
+	SayHi();
 }
