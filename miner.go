69a240c9db02bf405e738ccb44751864cbcdb57f
Brad Bowie <broanius@gmail.com>
2015-06-18 09:51:47 -0400

    Formatted via go fmt

--- miner.go
+++ miner.go
@@ -4,37 +4,37 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-    "os"
+	"os"
 )
 
 func ParseFile(fileName string, analytics *Analytics) {
-    file, err := os.Open(fileName)
+	file, err := os.Open(fileName)
 
-    if err != nil {
-        panic(err.Error())
-    }
+	if err != nil {
+		panic(err.Error())
+	}
 
-    defer file.Close()
+	defer file.Close()
 
-    reader := bufio.NewReader(file)
-    scanner := bufio.NewScanner(reader)
-    var data map[string]interface{}
+	reader := bufio.NewReader(file)
+	scanner := bufio.NewScanner(reader)
+	var data map[string]interface{}
 
-	fmt.Println("Mining JSON file: " + fileName);
-	skippedLines := 0;
-    scanner.Split(bufio.ScanLines)
-    for scanner.Scan() {
-		analytics.OnRow();
-        err := json.Unmarshal(scanner.Bytes(), &data)
+	fmt.Println("Mining JSON file: " + fileName)
+	skippedLines := 0
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		analytics.OnRow()
+		err := json.Unmarshal(scanner.Bytes(), &data)
 
-        if err != nil {
+		if err != nil {
 			skippedLines++
-        } else {
-		    analytics.OnData(data);
+		} else {
+			analytics.OnData(data)
 		}
 	}
 
 	if skippedLines > 0 {
-		fmt.Printf("Skipped %d non-JSON lines in '%s'.\n", skippedLines, fileName);
+		fmt.Printf("Skipped %d non-JSON lines in '%s'.\n", skippedLines, fileName)
 	}
 }
